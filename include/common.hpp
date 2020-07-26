52a5de2c9135bfb8da7637215ae8b3eb920a9e37
elvin <jinbin_tan@icloud.com>
2020-07-26 09:32:12 +0800

    add a parameters print function

--- include/common.hpp
+++ include/common.hpp
@@ -139,6 +139,28 @@ inline void consoleProgress(int progress, int begin, int end)
     consoleProgress(100. * (progress - begin) / (end - begin));
 }
 
+// 画一条固定长度的分割线(强迫症福利)
+inline void pLine()
+{
+    std::cout << "============================================================================" << std::endl;
+}
+
+// 打印参数列表，方便查错
+inline void printParam(const std::string &header, 
+    std::vector<std::pair<std::string, std::string>> &param)
+{
+    pLine();
+    std::cout << "\t\t\t" << header << std::endl;
+    pLine();
+
+    for(int i=0; i<param.size(); ++i)
+        std::cout << "\t" << param[i].first << " = " << param[i].second << std::endl;
+    
+    pLine();
+}
+
+
+
 class ArgParser
 {
 public:
