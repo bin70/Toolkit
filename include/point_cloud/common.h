6d5e423a7f4d88790f97da1e853331ffacd5ddf0
elvin <jinbin_tan@icloud.com>
2020-07-28 11:23:16 +0800

    re-organize code

--- include/point_cloud/common.h
+++ include/point_cloud/common.h
@@ -7,3 +7,8 @@
 //typedef pcl::PointXYZI PointType;
 typedef pcl::PointXYZINormal PointType;
 typedef pcl::PointCloud<PointType> PointCloud;
+
+inline float getDistance(PointType p1, PointType p2)
+{
+    return sqrt(pow(p1.x-p2.x, 2)+pow(p1.y-p2.y, 2)+pow(p1.z-p2.z, 2));
+}
