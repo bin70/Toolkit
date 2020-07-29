a2c524a2949cd211cac954c5492125e8f950141c
bin70 <jinbin_tan@icloud.com>
2020-07-29 09:42:15 +0800

    move consoleProgress to FileOperator

--- include/io/FileOperator.hpp
+++ include/io/FileOperator.hpp
@@ -8,11 +8,47 @@
 #include <fstream>
 #include <eigen3/Eigen/Dense>
 
+inline void consoleProgress(int progress)
+{
+    static char bar[102] = {0};
+    static std::string lable = "|/-\\";
+    static int inprogress = -1;
+    if (inprogress == progress)
+        return;
+
+    int st = progress / 2;
+    for (int i = 0; i < st; i++)
+        bar[i] = '#';
+
+    bar[st] = 0;
+
+    printf("[%-50s][%d%%][%c]\r", bar, progress, lable[progress % 4]);
+    fflush(stdout);
+
+    inprogress = progress;
+
+    if (progress == 100)
+        printf("\n");
+}
+
+inline void consoleProgress(int progress, int begin, int end)
+{
+    consoleProgress(100. * (progress - begin) / (end - begin));
+}
+
 class FileOperator{
     typedef std::string string;
     typedef Eigen::Matrix4d Matrix;
 
 public:
+    ///************************************************************************/
+    ///* 根据文件数打印处理进度，没有打开文件则打印 0%                               */
+    ///************************************************************************/
+    void printProgress(int file_index)
+    {
+        consoleProgress(file_index, 0, file_num);
+    }
+
     ///************************************************************************/
     ///* 递归创建文件夹，存在则不创建                                               */
     ///************************************************************************/
@@ -51,6 +87,10 @@ public:
             filelist.push_back(path + "/" + filename);
         }
         closedir(dir);
+
+        opened_dir = true;
+        file_num = filelist.size();
+
         return filelist;
     }
 
@@ -58,6 +98,15 @@ public:
     {
         std::string fullName = path.substr(path.rfind('/') + 1);
         return fullName.substr(0, fullName.find('.'));
+    }
+
+    ///***************************************
+    ///* 返回一个路径的父文件夹所在的路径
+    ///***************************************
+    std::string getParentDir(std::string path)
+    {
+        int path_end = path.rfind('/');
+        return path.substr(0, path_end);
     }
 
     Matrix loadMatrix(std::string path)
@@ -72,6 +121,9 @@ public:
     }
 
 private:
+    bool opened_dir = false;
+    int file_num = 0;
+
     bool CheckFileExist(const char *p)
     {
         if ((access(p, 0)) == 0)
